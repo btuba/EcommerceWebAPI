d6794df29932d4ac956228d102621c1c870c4b96
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-19 23:26:03 +0300

    Adds Scoped Services for EFEntityRepository Classes

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,6 @@
+using DataAccess;
 using DataAccess.Data;
+using DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// EFEntityRepository Configuration
+builder.Services.AddScoped<IProductRepository, EFProductRepository>();
+builder.Services.AddScoped<IAdrressRepository,EFAddressRepository >();
+builder.Services.AddScoped<ICustomerRepository,EFCustomerRepository >();
+builder.Services.AddScoped<ICategoryRepository, EFCategoryRepository >();
+builder.Services.AddScoped<IColorRepository, EFColorRepository>();
+builder.Services.AddScoped<IImageRepository, EFImageRepository>();
+builder.Services.AddScoped<IInventoryRepository, EFInventoryRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
+builder.Services.AddScoped<IPaymentRepository, EFPaymentRepository>();
+builder.Services.AddScoped<ISizeRepository, EFSizeRepository>();
 
 // Connect to Db
 builder.Services.AddDbContext<ApiDbContext>(opt =>
