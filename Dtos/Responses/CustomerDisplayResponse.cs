297e53d03fd220c4132c9bad4c5ebb2d62892d49
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-20 20:36:38 +0300

    Adds Dtos Objects

---
+++ Dtos/Responses/CustomerDisplayResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.Responses
+{
+    public class CustomerDisplayResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public ICollection<object> Addresses { get; set; }
+        public ICollection<object> Payments { get; set; }
+        public ICollection<object> Orders { get; set; }
+    }
+}
---
+++ Dtos/Responses/ImageDisplayResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.Responses
+{
+    public interface ImageDisplayResponse
+    {
+        public int Id { get; set; }
+        public object Color { get; set; }
+        public string Url { get; set; }
+    }
+}
