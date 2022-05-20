72a8f8cbc17426a4fcb660a018c94d486b9b8718
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-20 23:21:28 +0300

    Add Map Profile

---
+++ Business/Mapping/MapProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Address, AddressDisplayResponse>();
+            CreateMap<Category, CategoryDisplayResponse>();
+            CreateMap<Color, ColorDisplayResponse>();
+            CreateMap<Customer, CustomerDisplayResponse>();
+            CreateMap<Customer, CustomerListResponse>();
+            CreateMap<Image, ImageDisplayResponse>();
+            CreateMap<Inventory, InventoryDisplayResponse>();
+            CreateMap<Order, OrderDisplayResponse>();
+            CreateMap<Payment, PaymentDisplayResponse>();
+            CreateMap<Product, ProductDisplayResponse>();
+            CreateMap<Product, ProductCardDisplayResponse>();
+            CreateMap<Size, SizeDisplayResponse>();
+            
+            CreateMap<AddAddressRequest, Address>();
+            CreateMap<AddCategoryRequest, Category>();
+            CreateMap<AddColorRequest, Color>();
+            CreateMap<AddCustomerRequest, Customer>();
+            CreateMap<AddImageRequest, Image>();
+            CreateMap<AddInventoryRequest, Inventory>();
+            CreateMap<AddOrderRequest, Order>();
+            CreateMap<AddPaymentRequest, Payment>();
+            CreateMap<AddProductRequest, Product>();
+            CreateMap<AddSizeRequest, Size>();
+        }
+    }
+}
